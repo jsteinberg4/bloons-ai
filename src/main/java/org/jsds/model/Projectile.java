d812b0798bf3e7a7b179216abacaae942d5f28e9
deionjsm <deionjsm@gmail.com>
2022-11-01 21:24:18 -0400

    Commit

--- src/main/java/org/jsds/model/Projectile.java
+++ src/main/java/org/jsds/model/Projectile.java
@@ -9,8 +9,24 @@ public class Projectile {
     // damage dealt by a projectile
     int damage = 1;
 
-    // position of projectile
+    // position of projectile which is initialized as
     Coordinate projectilePosition;
 
-    
+    public Projectile(Coordinate pos) {
+        if(pos == null) {
+            throw new NullPointerException("Coordinate used to initialize projectile is null");
+        }
+        this.projectilePosition = pos;
+    }
+
+
+    // Get damage that will be dealt by projectile
+    public int getDamage() {
+        return this.damage;
+    }
+
+    // Get projectile position
+    public Coordinate getProjectilePosition() {
+        return this.projectilePosition;
+    }
 }
